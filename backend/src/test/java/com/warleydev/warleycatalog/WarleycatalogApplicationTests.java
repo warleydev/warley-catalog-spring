24c18306508b6797e25f9955012d7a5d42a4a466
Warley Dev <warleycm7@gmail.com>
2023-05-23 15:15:44 -0300

    projeto criado

---
+++ backend/src/test/java/com/warleydev/warleycatalog/WarleycatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warleydev.warleycatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarleycatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
