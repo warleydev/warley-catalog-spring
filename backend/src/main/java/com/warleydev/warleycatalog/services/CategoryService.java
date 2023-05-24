caf08ff2e6a7dc73363ac72ca11d9c1c676f6822
Warley Dev <warleycm7@gmail.com>
2023-05-23 22:06:28 -0300

    implementacao do modelo de camadas, e conectando o banco de testes H2

---
+++ backend/src/main/java/com/warleydev/warleycatalog/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.warleydev.warleycatalog.services;
+
+import com.warleydev.warleycatalog.entities.Category;
+import com.warleydev.warleycatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    @Transactional
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+}
