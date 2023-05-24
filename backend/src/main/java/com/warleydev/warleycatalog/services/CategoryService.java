b7a3d1e88ab0340a91ecbf88bd33ab7c8e80421d
Warley Dev <warleycm7@gmail.com>
2023-05-23 22:26:49 -0300

    populando o banco de dados

--- backend/src/main/java/com/warleydev/warleycatalog/services/CategoryService.java
+++ backend/src/main/java/com/warleydev/warleycatalog/services/CategoryService.java
@@ -14,7 +14,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
-    @Transactional
+    @Transactional(readOnly = true)
     public List<Category> findAll(){
         return repository.findAll();
     }
