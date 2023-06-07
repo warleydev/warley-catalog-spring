b1d1c26432e7a3e35689619e10206fc26af42209
Warley Dev <warleycm7@gmail.com>
2023-06-07 18:11:37 -0300

    refatoraçao no Pageable

--- backend/src/main/java/com/warleydev/warleycatalog/resources/ProductResource.java
+++ backend/src/main/java/com/warleydev/warleycatalog/resources/ProductResource.java
@@ -6,6 +6,7 @@ import com.warleydev.warleycatalog.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,16 +21,9 @@ public class ProductResource {
     private ProductService service;
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(
-            @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
-            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-        ){
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){
 
-        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
-
-        return ResponseEntity.ok().body(service.findAllPaged(pageRequest));
+        return ResponseEntity.ok().body(service.findAllPaged(pageable));
     }
 
     @GetMapping(value = "/{id}")
--- backend/src/main/java/com/warleydev/warleycatalog/services/ProductService.java
+++ backend/src/main/java/com/warleydev/warleycatalog/services/ProductService.java
@@ -11,7 +11,7 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,8 +29,8 @@ public class ProductService {
 
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAllPaged(PageRequest pageRequest){
-        Page<Product> list = repository.findAll(pageRequest);
+    public Page<ProductDTO> findAllPaged(Pageable pageable){
+        Page<Product> list = repository.findAll(pageable);
         return list.map(product -> new ProductDTO(product, product.getCategories()));
     }
 
