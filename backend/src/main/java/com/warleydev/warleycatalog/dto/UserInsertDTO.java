2db0ef1f3e5a716e982e8bd9606dc60b3fd546a2
Warley Dev <warleycm7@gmail.com>
2023-07-11 20:17:48 -0300

    ajustando configurações de segurança p1

--- backend/src/main/java/com/warleydev/warleycatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/warleydev/warleycatalog/dto/UserInsertDTO.java
@@ -2,11 +2,13 @@ package com.warleydev.warleycatalog.dto;
 
 import com.warleydev.warleycatalog.services.validation.UserInsertValid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @UserInsertValid
 public class UserInsertDTO extends UserDTO{
 
     @NotBlank(message = "Este campo não pode estar vazio.")
+    @Size(min = 8, message = "A senha deve possuir no mínimo 8 caracteres")
     private String password;
 
     public UserInsertDTO(){
--- backend/src/main/java/com/warleydev/warleycatalog/services/UserService.java
+++ backend/src/main/java/com/warleydev/warleycatalog/services/UserService.java
@@ -15,12 +15,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -84,4 +87,14 @@ public class UserService {
         }
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByEmail(username);
+        if (user == null){
+            logger.error("Usuário não encontrado: "  + username);
+            throw new UsernameNotFoundException("Email não encontrado!");
+        }
+        logger.info("Usuário encontrado: " + username);
+        return user;
+    }
 }
